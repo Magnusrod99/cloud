ee1a4d30e8efc64b54257980aa01b72c7f4c8146
yash gaonkar <yashgaonkar@2005>
2025-10-02 19:15:31 +0530

    Update db.php to connect to Azure MySQL

--- db.php
+++ db.php
@@ -1,5 +1,11 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "campusvoice");
+$host = "collegecomplaints-db.mysql.database.azure.com"; // Azure server name
+$username = "admin@collegecomplaints-db";               // admin username + @server-name
+$password = "P#8vR9t!s2QwZ6xL";                         // the strong password you chose
+$database = "campusvoice";
+
+$conn = new mysqli($host, $username, $password, $database);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
